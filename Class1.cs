9a058c27773939fe37af24687e9f4f6140d450a5
d11116228-svg <d11116228@gs.takming.edu.tw>
2026-04-07 16:05:03 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿class Person
+{
+    public string name;
+    public int Id;
+
+    public Person()
+    {
+        name = "預設名字";
+    }
+
+    public Person(int id, string name)
+    {
+        Id = id;
+        this.name = name;
+    }
+
+    public string Info()
+    {
+        return "Person 的 info";
+    }
+
+    public string GetName()
+    {
+        return "";
+    }
+}
+}
+class Student : Person
+{
+    private int Score;
+
+    public Student(int id, string name, int score) : base(id, name)
+    {
+        Score = score;
+    }
+
+    public int Grade
+    {
+        get
+        {
+            return Score;
+        }
+    }
+
+    public string StudentInfo()
+    {
+        return "學生的新方法";
+    }
+
+    public string Info()
+    {
+        return "學生類別自己的 info";
+    }
+}
